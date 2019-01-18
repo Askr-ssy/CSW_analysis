d96383b32848e9520e0decfcf928d89ef32f8e7a
askr-ssy <ask.shen@1data.info>
2019-01-18 15:21:23 +0800

    add

--- jieba_test.py
+++ jieba_test.py
@@ -1,8 +1,9 @@
 import os
+
 import jieba
 import json
 
-from _utils import find_all_file
+from _utils import find_all_file,cut_filename
 
 ROOT=os.path.dirname(os.path.abspath(__file__))
 fc =lambda str:" ".join(jieba.cut(str))
@@ -12,13 +13,13 @@ def get_target_file(need_TW = False):
     '''
     生成测试文件列表
     '''
-    for file in find_all_file(path(ROOT,'test')):
-        name = file.split(".")
-        if name[1] == "utf8":
-            if not "tw" in name[0]:
-                yield os.path.join(dir_path,"test",file)
-            elif "tw" in name[0] and need_TW:
-                yield os.path.join(dir_path,"test",file)
+    for file_path in find_all_file(path(ROOT,'test'),suffix_filter='utf8'):
+        name = cut_filename(file_path)
+        if name[2] == ".utf8":
+            if not "tw" in name[1]:
+                yield file_path
+            elif "tw" in name[1] and need_TW:
+                yield file_path
 
 if __name__ == "__main__":
     '''
@@ -26,15 +27,15 @@ if __name__ == "__main__":
     '''
     jieba.initialize()
     for file_address in get_target_file():
+        print('file_address',file_address)
         out = []
         with open(file_address,"r",encoding="utf-8") as file:
             for line in file.readlines():
                 out.append(fc(line))
-        res_address = file_address.replace("test","res")
-        isExists = os.path.exists(os.path.split(res_address)[0])
+        res_address = file_address.replace('test','res')
+        isExists = os.path.exists(cut_filename(res_address)[0])
         if not isExists:
             os.makedirs(os.path.split(res_address)[0])
-        print(res_address)
         with open(res_address,"w",encoding="utf-8")as file:
             file.writelines(out)
             file.write("\nstop code\n")
