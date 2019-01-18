1640fd7840d25610bbff4ff453b0dbf870874f3f
askr-ssy <ask.shen@1data.info>
2019-01-18 14:51:54 +0800

    Update jieba_test.py

--- jieba_test.py
+++ jieba_test.py
@@ -13,18 +13,18 @@ def get_target_file(need_TW = False):
             elif "tw" in name[0] and need_TW:
                 yield os.path.join(dir_path,"test",file)
 
-
-jieba.initialize()
-for file_address in get_target_file():
-    out = []
-    with open(file_address,"r",encoding="utf-8") as file:
-        for line in file.readlines():
-            out.append(fc(line))
-    res_address = file_address.replace("test","res")
-    isExists = os.path.exists(os.path.split(res_address)[0])
-    if not isExists:
-        os.makedirs(os.path.split(res_address)[0])
-    print(res_address)
-    with open(res_address,"w",encoding="utf-8")as file:
-        file.writelines(out)
-        file.write("\nstop code\n")
+if __name__ == "__main__":
+    jieba.initialize()
+    for file_address in get_target_file():
+        out = []
+        with open(file_address,"r",encoding="utf-8") as file:
+            for line in file.readlines():
+                out.append(fc(line))
+        res_address = file_address.replace("test","res")
+        isExists = os.path.exists(os.path.split(res_address)[0])
+        if not isExists:
+            os.makedirs(os.path.split(res_address)[0])
+        print(res_address)
+        with open(res_address,"w",encoding="utf-8")as file:
+            file.writelines(out)
+            file.write("\nstop code\n")
